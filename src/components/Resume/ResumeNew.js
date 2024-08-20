867935c8a58a9313b463b20a821aba85adba2759
hamzahafaiedh <hamza.hafaiedh@esprit.tn>
2024-08-20 18:44:47 +0200

    update pdf

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "./CV_hamza_hafaiedh.pdf";
+import pdf from "./Cv_Hafaiedh_Hamza.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
